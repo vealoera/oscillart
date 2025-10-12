9476bdebc423f2302277d7e9ba355b0b43a63486
Vera Qian <veraqian101@gmail.com>
2025-10-12 00:49:05 -0400

    part 4 donedonedone!!!

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 var width = ctx.canvas.width;
 var height = ctx.canvas.height;
+var reset = false;
+var timepernote = 0;
+var length = 0;
 
 notenames = new Map();
 notenames.set("C", 261.6);
@@ -29,36 +32,63 @@ function frequency(pitch) {
     freq = (pitch / 10000);
     gainNode.gain.setValueAtTime(100, audioCtx.currentTime);
     oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
-    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + 1);
+    gainNode.gain.setValueAtTime(0, audioCtx.currentTime + (timepernote/1000)-0.1);
 }
 
 function handle() {
+    reset = true;
     audioCtx.resume();
     gainNode.gain.value = 0;
     var usernotes = String(input.value);
-    frequency(notenames.get(usernotes));
+    var noteslist = [];
+    length = usernotes.length;
+    timepernote = (6000 / length);
+
+    for (i = 0; i < usernotes.length; i++)  {
+        noteslist.push(notenames.get(usernotes.charAt(i)));
+    }
+
+    let j = 0;
+
+    frequency(parseInt(noteslist[j]));
     drawWave();
+    j++;
+    
+    let repeat;
+    repeat = setInterval(() => {
+        if (j < noteslist.length) {
+            frequency(parseInt(noteslist[j]));
+            drawWave();
+        j++
+        } else {
+            clearInterval(repeat)
+        }
+    }, timepernote)
 }
 
 var counter = 0;
 function drawWave() {
-    ctx.clearRect(0, 0, width, height);
-    x = 0;
-    y = height/2;
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    clearInterval(interval);
+    if (reset) {
+        ctx.clearRect(0, 0, width, height);
+        x = 0;
+        y = height/2;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    }
     counter = 0;
     interval = setInterval(line, 20);
+    reset = false;
 }
 
 var counter = 0;
 function line() {
-    y = height/2 + (amplitude * Math.sin(x * 2 * Math.PI * freq));
+    y = height/2 + amplitude * Math.sin(x * 2 * Math.PI * freq * (0.5 * length));
     ctx.lineTo(x, y);
     ctx.stroke();
     x = x + 1;
     counter++;
-    if (counter > 50) {
+    if (counter > (timepernote/20)) {
         clearInterval(interval);
     }
 }
